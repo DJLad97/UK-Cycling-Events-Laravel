be122fe165d4f710d2cc3e7915735943dc392916
Dan <b5014133@my.shu.ac.uk>
2017-10-29 13:51:29 +0000

    Test commit 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,7 +80,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    <span class="test">Laravel - HELLO WORLD </span>
+                    <span class="test">Laravel - HELLO WORLD</span>
                 </div>
 
                 <div class="links">
