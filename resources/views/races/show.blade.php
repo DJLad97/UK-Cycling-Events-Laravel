68c347ba95df864ddef7bd927883b9d1c818c801
Dan <b5014133@my.shu.ac.uk>
2017-10-31 21:17:42 +0000

    Added models for corresponding tables, created views for races and individual race with funcionallity, create race controller that passes data to the views

---
+++ resources/views/races/show.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <li>{{ $race->raceName }}</li>
+    <li>{{ $race->raceDate }}</li>
+    <li>{{ $race->raceAddress }}</li>
+    <li>{{ $race->raceType }}</li>
+    @if ($race->isFree === 1)
+        <li>FREE!</li>
+    @else
+        <li>{{ $race->entryPrice }}</li>
+    @endif
+@endsection
