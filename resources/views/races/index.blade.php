bc491d08013625341cb725c3fa4307587a41a51b
Dan <b5014133@my.shu.ac.uk>
2017-11-01 18:59:18 +0000

    Removed conflict stuff

--- resources/views/races/index.blade.php
+++ resources/views/races/index.blade.php
@@ -2,14 +2,6 @@
 
 @section('content')
     @foreach($races as $race)
-<<<<<<< HEAD
         <li><a href="races/show/{{$race->raceID}}">{{ $race->raceName }}</a></li>
-=======
-<<<<<<< HEAD
-        <li><a href="races/show/{{$race->raceID}}">{{ $race->raceName }}</a>test</li>
-=======
-        <li><a href="races/show/{{$race->raceID}}">{{ $race->raceName }}</a></li>
->>>>>>> 68c347ba95df864ddef7bd927883b9d1c818c801
->>>>>>> b7e0537190866a3b18cf0b9f1abdeb556f2afda2
     @endforeach
 @endsection
