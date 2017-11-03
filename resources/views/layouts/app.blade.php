bf975af983ac95d64965f352eb79c1072556c683
Dan <b5014133@my.shu.ac.uk>
2017-11-03 23:15:43 +0000

    Removed original login and register mini nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,91 +7,21 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    
 
     <title>{{ config('app.name', 'UK Cycling Events') }}</title>
 
     <!-- Styles -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/nav.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/meanmenu.min.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div class="container">
-    <div id="login-modal" class="modal">
-        <div class="col-xs-1 col-sm-3 col-md-4"></div>
-        <form class="model-content animate col-xs-10 col-sm-6 col-md-4" method="post" action="{{route('login')}}" id="login-form">
-            {{csrf_field() }}
-            <span onclick="document.getElementById('login-modal').style.display='none'" class="close" title="Close Modal">&times;</span>
-            <div id="error">
-            </div>
-            <div class="page-header">
-                <h2>SIGN IN</h2>
-            </div>
-            {{--  <div class="form-group">
-                <label>Username</label>
-                <input type="text" class="text-box" name="email" placeholder="Username"/>
-            </div>  --}}
-            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                <label for="email" class="control-label">E-Mail Address</label>
-                <input id="email" type="email" class="text-box" name="email" value="{{ old('email') }}" placeholder="Email" required autofocus>
+    @include('layouts.nav')
+    @include('layouts.modals')
 
-                @if ($errors->has('email'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('email') }}</strong>
-                    </span>
-                @endif
-            </div>
-            {{--  <div class="form-group">
-                <label>Password</label>
-                <input type="password" class="text-box" name="password" placeholder="Password"/>
-            </div>  --}}
-            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                <label for="password" class="control-label">Password</label>
-
-                <input id="password" type="password" class="text-box" name="password" placeholder="Password" required>
-
-                @if ($errors->has('password'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('password') }}</strong>
-                    </span>
-                @endif
-            </div>
-            <p><a href="sign-up.php">Don't have an account?</a></p>
-            <div class="form-group">
-                <input type="submit" name="submit" value="Sign In" class="modal-btn" />
-            </div>
-        </form>
-        <div class="col-xs-1 col-sm-3 col-md-4"></div>
-    </div>
-    <ul class="nav navbar-nav navbar-right">
-        <!-- Authentication Links -->
-        @guest
-            <li><a id="login">Login</a></li>
-            <li><a href="{{ route('register') }}">Register</a></li>
-        @else
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                    {{ Auth::user()->name }} <span class="caret"></span>
-                </a>
-
-                <ul class="dropdown-menu">
-                    <li>
-                        <a href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
-                                        document.getElementById('logout-form').submit();">
-                            Logout
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            {{ csrf_field() }}
-                        </form>
-                    </li>
-                </ul>
-            </li>
-        @endguest
-    </ul>
-        @include('layouts.nav')
+    <div class="container" id="container-content">
         @yield('content')
     </div>
     {{--  <div id="app">
@@ -152,7 +82,11 @@
 
         @yield('content')
     </div>  --}}
+
     <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/jquery.meanmenu.min.js') }}"></script>
 </body>
 </html>
