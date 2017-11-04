44a032bdd50e63e25ea25cc4b9a13930e6d2e8c6
Dan <b5014133@my.shu.ac.uk>
2017-11-04 21:22:05 +0000

    Addded route for ajax call

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,5 @@ Route::get('/', 'HomeController@index')->name('home');
 Route::get('/races', 'RacesController@index')->name('races');
 Route::get('/races/{race}', 'RacesController@show');
 
+Route::post('/raceSearch', 'RacesController@search');
 Route::post('/raceSignUps', 'RaceSignUpsController@store');
