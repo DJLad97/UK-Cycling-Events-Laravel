1f027eea31b657606017d7e1689ad01e4639d6ed
Dan <b5014133@my.shu.ac.uk>
2017-11-04 21:15:05 +0000

    Added function for live search to return races that is LIKE query, added relationships to comments and race sign ups, added function to check if user has signed up to race

--- app/Race.php
+++ app/Race.php
@@ -3,13 +3,39 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Race extends Model
 {
-    protected $primaryKey = "raceID";
-
     public static function raceList()
     {
         return static::All();
+    }
+
+    public static function raceLike($searchTerm)
+    {
+        return static::where('raceName', 'like', '%' . $searchTerm . '%')->get();
+    }
+
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function raceSignUps()
+    {
+        return $this->hasMany(RaceSignUp::class);
+    }
+
+    public function hasUserSignUp($raceID)
+    {
+        $signedUp = false;
+        $raceIDs = RaceSignUp::where('user_id', Auth::id())->pluck('race_id')->toArray();
+
+        if(in_array($raceID, $raceIDs))
+            $signedUp = true;
+
+        return $signedUp;
     }
 }
