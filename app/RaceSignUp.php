14a9a6cee170d31c9a2884141777eb58ac4080e2
Dan <b5014133@my.shu.ac.uk>
2017-11-04 21:21:30 +0000

    Added relationship to races and users table

--- app/RaceSignUp.php
+++ app/RaceSignUp.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class RaceSignUp extends Model
 {
     protected $guarded = [''];
+
+    public function races()
+    {
+        return $this->hasMany(Race::class);
+    }
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
 }
