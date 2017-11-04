a4a0a94e80d052b9e009b0dea33ac4016ccdde16
Dan <b5014133@my.shu.ac.uk>
2017-11-04 21:22:50 +0000

    Added if statement to check if the user has already signed up to that race

--- resources/views/races/race.blade.php
+++ resources/views/races/race.blade.php
@@ -12,34 +12,42 @@
     @endif
 
     @guest
-    <p>Please <a href="{{route('login')}}">log in</a> or <a href="{{route('register')}}">create an account</a> to sign up</p>
+    <p>Please <a class="login a-pointer">log in</a> or <a href="{{route('register')}}">create an account</a> to sign up</p>
     @else
-        <form action="/raceSignUps" method="post">
-            {{ csrf_field() }}
-            <input type="hidden" name="raceID" value="{{$race->raceID}}">
-            <div class="form-group">
-                {{--  <select class="form-control" name="gender" required>
-                    <option value="M">M</option>
-                    <option value="F">F</option>
-                </select>  --}}
-                <label for="gender">Gender</label>
-                <input type="radio" name="gender" checked="checked" value="M" required>
-                    Male
-                </input>
-                <input type="radio" name="gender" checked="checked" value="F" required>
-                    Female
-                </input>
-            </div>
-            <div class="form-group">
-                <select class="form-control" name="ageRange" required>
-                    <option value="14-17">14-17</option>
-                    <option value="18-39">18-39</option>
-                    <option value="40+">40+</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Sign Up">
-            </div>
-        </form>
+        @if(!$race->hasUserSignUp($race->id))
+            <form action="/raceSignUps" method="post">
+                {{ csrf_field() }}
+                <input type="hidden" name="race_id" value="{{$race->id}}">
+                <div class="form-group">
+                    {{--  <select class="form-control" name="gender" required>
+                        <option value="M">M</option>
+                        <option value="F">F</option>
+                    </select>  --}}
+                    <label for="gender">Gender</label>
+                    <input type="radio" name="gender" checked="checked" value="M" required>
+                        Male
+                    </input>
+                    <input type="radio" name="gender" value="F" required>
+                        Female
+                    </input>
+                </div>
+                <div class="form-group">
+                    <select class="form-control" name="ageRange" required>
+                        <option value="14-17">14-17</option>
+                        <option value="18-39">18-39</option>
+                        <option value="40+">40+</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="name">Name: </label>
+                    <input type="text" name="name" id="name" class="text-box" value="{{Auth::user()->name}}">
+                </div>
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" value="Sign Up">
+                </div>
+            </form>
+        @else
+            <h2>YOU'VE ALREADY SIGNED UP</h2>
+        @endif
     @endguest
 @endsection
