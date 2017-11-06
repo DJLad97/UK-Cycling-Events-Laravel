3978670c08c247c0d5ecd8b39e9b0a26b4a6b890
Dan <b5014133@my.shu.ac.uk>
2017-11-06 21:25:51 +0000

    Added id to removeItem js function and added a  way to sign up to race directly in the race list (this is only temporary, but it was easier to do this for cart testing purposes

--- resources/views/races/index.blade.php
+++ resources/views/races/index.blade.php
@@ -6,7 +6,7 @@
             <li>
                 {{ $item['name'] }}
                 {{ $item['price'] }}
-                <input type="button" onclick="removeItem()" class="btn btn-danger" name="removeItem" id="removeItem" value="Remove">
+                <input type="button" onclick="removeItem({{$item['id']}})" class="btn btn-danger" name="removeItem" id="removeItem" value="Remove">
             <li>
         @endforeach
     </div>
@@ -16,7 +16,17 @@
     <ul id="content"></ul>
     <div id="initial-results">
         @foreach($races as $race)
+        <form action="/raceSignUps" method="post">
+                {{ csrf_field() }}
+            <input type="hidden" name="race_id" value="{{$race->id}}">
+            <input type="hidden" name="gender" value="M">
+            <input type="hidden" name="ageRange" value="18-39">
+            <input type="hidden" name="name" value="Dan Scott">
             <li><a href="/races/{{$race->id}}">{{ $race->raceName }}</a></li>
+        <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Sign Up">
+            </div>
+        </form>
         @endforeach
     </div>
 @endsection
