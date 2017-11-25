3a6b3855e962601e09f7c3e4a9c59c73175e32a6
Dan <b5014133@my.shu.ac.uk>
2017-11-25 19:40:39 +0000

    Added profile scss to webpack file

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .js('resources/assets/js/races-map.js', 'public/js')
-   .js('resources/assets/js/live-race-search.js', 'public/js')
+   .js('resources/assets/js/ajax.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
+   .sass('resources/assets/sass/profile.scss', 'public/css')
    .sass('resources/assets/sass/nav.scss', 'public/css');
