f68b70a005704b03c3c95578e8bc651e21f09e36
Dan <b5014133@my.shu.ac.uk>
2017-11-25 19:44:22 +0000

    Removed the cart test thing from races page

--- resources/views/races/index.blade.php
+++ resources/views/races/index.blade.php
@@ -1,26 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="dropdown">
-        <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">
-            CART<span class="caret"></span>
-        </button>
-        <ul class="dropdown-menu">
-            @foreach($cart as $item)
-                <li>
-                    {{ $item['name'] }}
-                    {{ $item['price'] }}
-                    <input type="button" onclick="removeItem({{$item['id']}})" class="btn btn-danger" name="removeItem" id="removeItem" value="Remove">
-                <li>
-            @endforeach
-        </ul>
-    </div>
-    <div class="cart">
-        <ul id="cart-items">
-        </ul>
-    </div>
-    {{--  {{ var_dump($cart) }}  --}}
-    {{--  {{ Session::get('cart')->Name}}  --}}
     <input type="text" class="text-box" id="searchTerm" name="searchTerm" autocomplete="off" placeholder="Race Name/Type">
     <ul id="content"></ul>
     <div id="initial-results">
