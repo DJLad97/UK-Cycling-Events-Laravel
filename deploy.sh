7c19862d304a33d4c8e6be9d414d88eddc6d7134
Dan <b5014133@my.shu.ac.uk>
2018-06-23 22:07:38 +0100

    deploy changes

--- deploy.sh
+++ deploy.sh
@@ -5,16 +5,15 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  cd public
   git init
 
-  git remote add deploy "travis@danjscott.co.uk:/var/www/ukce.danjscott.co.uk/"
   git config user.name "Dan"
   git config user.email "b5014133@my.shu.ac.uk"
 
-  git add .
-  git commit -m "Deploy"
-  git push --force deploy master
+  git pull origin master
+  # git add .
+  # git commit -m "Deploy"
+  # git push --force deploy master
 else
   echo "Not deploying, since this branch isn't master."
 fi
