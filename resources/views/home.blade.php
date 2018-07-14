87dd09bea9129082c4402b1a4b0fb41b55731cfb
Dan <b5014133@my.shu.ac.uk>
2018-07-14 14:31:03 +0100

    removed test code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<h2>UPCOMING RACE CAROUSEL deploy test</h2>
+<h2>UPCOMING RACE CAROUSEL</h2>
 <h2>UPCOMING RACE LIST</h2>
 <h3>MTB RACES | ROAD RACES</h3>
 <h3>MTB RACE MAP | ROAD RACE MAP</h3>
