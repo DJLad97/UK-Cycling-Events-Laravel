c22abb11ae0a32ec1128fef122c466707a97df28
Dan <b5014133@my.shu.ac.uk>
2018-08-22 17:57:23 +0100

    Added error messages onto the inputs

--- resources/assets/js/components/Register.vue
+++ resources/assets/js/components/Register.vue
@@ -11,24 +11,28 @@
 
                 <div class="modal-body" id="reg-name-input">
                     <div class="form-group">
-                        <label for="name">Name</label>
-                        <input id="name" v-model="name" type="text" class="text-box" name="name" value="" required autofocus>
+                        <label for="name">Name</label>*
+                        <input id="name" v-model="name" type="text" :class="{'input-error': errors.name, 'text-box': true}" name="name" placeholder="Name" required autofocus>
+                        <span v-if="errors.name" class="input-error-message">{{errors.name}}</span>
                     </div>
                     <div class="form-group" id="reg-username-input">
-                        <label for="username">Username</label>
-                        <input id="username" v-model="username" type="text" class="text-box" name="username" value="" required autofocus>
+                        <label for="username">Username</label>*
+                        <input id="username" v-model="username" type="text" :class="{'input-error': errors.username, 'text-box': true}" name="username" placeholder="Username" required autofocus>
+                        <span v-if="errors.username" class="input-error-message">{{errors.username}}</span>
                     </div>
                     <div class="form-group" id="reg-email-input">
-                        <label for="email">E-Mail Address</label>
-                        <input id="email" v-model="email" type="email" class="text-box" name="email" value="" required>
+                        <label for="email">E-Mail Address</label>*
+                        <input id="email" v-model="email" type="email" :class="{'input-error': errors.email, 'text-box': true}" name="email" placeholder="Email" required>
+                        <span v-if="errors.email" class="input-error-message">{{errors.email}}</span>
                     </div>
                     <div class="form-group" id="reg-password-input">
-                        <label for="password">Password</label>
-                        <input id="password" v-model="password" type="password" class="text-box" name="password" required>
+                        <label for="password">Password</label>*
+                        <input id="password" v-model="password" type="password" :class="{'input-error': errors.password, 'text-box': true}" name="password" placeholder="Password" required>
+                        <span v-if="errors.password" class="input-error-message">{{errors.password}}</span>
                     </div>
                     <div class="form-group" id="reg-passwordconf-input">
-                        <label for="password-confirm">Confirm Password</label>
-                        <input id="password-confirm" v-model="passwordConfirm" type="password" class="text-box" name="password_confirmation" required>
+                        <label for="password-confirm">Confirm Password</label>*
+                        <input id="password-confirm" v-model="passwordConfirm" type="password" :class="{'input-error': errors.password, 'text-box': true}" name="password_confirmation" placeholder="Password Confirmation" required>
                     </div>
                     <!-- <div class="row">
                         <div class="col-md-4">
@@ -54,7 +58,10 @@
                 username: '',
                 email: '',
                 password: '',
-                passwordConfirm: ''
+                passwordConfirm: '',
+                errors: {},
+                errorClass: 'input-error',
+                textBoxClass: 'text-box'
             }
         },
 
@@ -70,8 +77,14 @@
                     .then(function (response) {
                         location.reload();
                     })
-                    .catch(function (error) {
-                        console.log(error.response);
+                    .catch((error) => {
+                        this.errors = {};
+                        var recievedErrors = error.response.data.errors;
+                        Object.keys(recievedErrors).forEach(element => {
+                            this.errors[element] = recievedErrors[element][0];
+                        });
+
+                        console.log(this.errors.username);
                     });
             }
         }
